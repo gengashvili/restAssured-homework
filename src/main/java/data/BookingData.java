4762ef3e45b02eb43c8cb5a162ce7e4250e47e71
giorgi gengashvili <giorgi.gengashvili0@gmail.com>
2023-10-28 00:26:47 +0400

    finish third task

---
+++ src/main/java/data/BookingData.java
@@ -0,0 +1,9 @@
+package data;
+
+public class BookingData {
+    public static final String
+            baseUri = "https://restful-booker.herokuapp.com",
+            basePath = "/booking",
+            userName = "admin",
+            password = "password123";
+}
